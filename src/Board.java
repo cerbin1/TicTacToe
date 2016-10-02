c120463bd8e93c05f8be7e40d0a5a74b2fb448ae
cerbin <bleble@d.com>
2016-10-02 17:23:44 +0200

    clean, make methods local

--- src/Board.java
+++ src/Board.java
@@ -2,49 +2,49 @@ class Board {
     private char[] charBoard;
     private boolean[] booleanBoard;
 
-    public Board() {
+    Board() {
         initializeCharBoard();
         initializeBooleanBoard();
     }
 
 
-    public void initializeCharBoard() {
+    private void initializeCharBoard() {
         charBoard = new char[9];
     }
 
-    public void fillCharBoard() {
+    void fillCharBoard() {
         for (int i = 0; i < charBoard.length; i++) {
             charBoard[i] = (char) (i + 49);
         }
     }
 
-    public char[] getCharBoard() {
+    char[] getCharBoard() {
         return charBoard;
     }
 
-    public void initializeBooleanBoard() {
+    private void initializeBooleanBoard() {
         booleanBoard = new boolean[9];
     }
 
-    public void fillBooleanBoard() {
+    void fillBooleanBoard() {
         for (int i = 0; i < booleanBoard.length; i++) {
             booleanBoard[i] = false;
         }
     }
 
-    public boolean getBooleanBoardValueAtIndex(int index) {
+    boolean getBooleanBoardValueAtIndex(int index) {
         return booleanBoard[index - 1];
     }
 
-    public void setBooleanBoardTrueAtIndex(int index) {
+    void setBooleanBoardTrueAtIndex(int index) {
         booleanBoard[index - 1] = true;
     }
 
-    public void setCircle(int index) {
+    void setCircle(int index) {
         charBoard[index - 1] = 'o';
     }
 
-    public void setCross(int index) {
+    void setCross(int index) {
         charBoard[index - 1] = 'x';
     }
 
