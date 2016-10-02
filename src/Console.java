51b1ec28a7c072d2f470b611a50b212e9e0e9e42
cerbin <bleble@d.com>
2016-10-02 13:08:55 +0200

    Added hello message and display instructions.

--- src/Console.java
+++ src/Console.java
@@ -2,4 +2,8 @@ public class Console {
     void displayHelloMessage() {
         System.out.println("Welcome in the TicTacToe game");
     }
+
+    void displayInstructions() {
+        System.out.println("Enter the number of index where you want to put sign.");
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -3,3 +3,38 @@ public class Game {
 
     }
 }
+
+/*
+
+class Border {
+    boolean[] booleanBoard;
+
+    public Border() {
+    this.booleanBoard = new boolean[9];
+    initalizeBooleanBoard();
+}
+
+    public boolean getValueAtIndex(int index) {
+        if (index >= booleanBoard.lenght) // throw error nie wiem jak xD
+            return booleanBoard[index];
+    }
+
+    public void setValueAtIndex(int index, boolean value) {
+        if (index >= booleanBoard.lenght) // throw error nie wiem jak xD
+
+            this.booleanBoard[index] = value;
+    }
+
+    private void initalizeBooleanBoard() {
+        for (int i = 0; i < booleanBoard.length; i++) {
+            booleanBoard[i] = false;
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Border boerd = new Border();
+        border.setValueAtIndex(5, true);
+    }
+}*/
