0f99d1a308da2f03d70dbf0517c1d62273642f8d
cerbin <bleble@d.com>
2016-10-03 18:50:36 +0200

    Inlined new Game() in class Application.

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,5 @@
 public class Application {
-    public static void main(String[] args) {
-        Game game = new Game();
-        game.run(); // TODO można zrobic inline
+    public static void main(String... arguments) {
+        new Game().run();
     }
 }
